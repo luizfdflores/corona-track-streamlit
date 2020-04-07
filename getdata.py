11f6351d4521d3119c59a91bb429bbbf6a134710
Luiz Felipe Diez Flores <luizfdflores@gmail.com>
2020-04-07 15:10:42 -0300

    Update to comply with PEP8 and fix line chart exception

--- getdata.py
+++ getdata.py
@@ -1,11 +1,8 @@
 import requests
 import pandas as pd
 import numpy as np
-import json
 from pandas import json_normalize
 from scipy.optimize import curve_fit
-import plotly.express as px
-import plotly.graph_objects as go
 from time import gmtime, strftime
 import streamlit as st
 
@@ -13,39 +10,40 @@ import streamlit as st
 base_url = 'http://corona-api.com/countries'
 
 
-def getCountryList():
+def getcountrylist():
     response = requests.get(base_url).json()
-    countryListCode = {}
+    countrylistcode = {}
     for country in response['data']:
-        countryListCode[country['name']] = country['code']
-    return countryListCode
+        countrylistcode[country['name']] = country['code']
+    return countrylistcode
 
 
-def getCountryData(countryCode):
-    url = f'{base_url}/{countryCode}'
+def getcountrydata(countrycode):
+    url = f'{base_url}/{countrycode}'
     response = requests.get(url).json()
     df = json_normalize(response['data'])
-    countryName = df['name'].values[0]
+    countryname = df['name'].values[0]
     df_timeline = pd.concat(json_normalize(data) for data in df['timeline'].values.tolist()[0])
-    df_timeline['name'] = countryName
+    df_timeline['name'] = countryname
     df_full = df.merge(df_timeline, on=['name']).drop('timeline', axis=1)
     return df_full
 
 
-def getAllData(countryListCode):
-    for i, countryName in enumerate(list(countryListCode.keys())):
+def getalldata(countrylistcode):
+    for i, countryName in enumerate(list(countrylistcode.keys())):
         try:
-            df_data = getCountryData(countryListCode[countryName])
-            _,_,df = SimpleStatistics(df_data, False)
+            df_data = getcountrydata(countrylistcode[countryName])
+            _, _, df = simplestatistics(df_data, False)
             df['country'] = countryName
             df['population'] = df_data['population']
-            if i==0:
+            if i == 0:
                 df_static = df
             else:
                 df_static = pd.concat([df_static, df])
         except:
             pass
     return df_static
+
 
 def logistic(t, a, b, c, d):
     return c + (d - c)/(1 + a * np.exp(- b * t))
@@ -54,7 +52,8 @@ def logistic(t, a, b, c, d):
 def exponential(t, a, b, c):
     return a * np.exp(b * t) + c
 
-def SimpleStatistics(data, print=True):
+
+def simplestatistics(data, print_stats=True):
 
     currdate = strftime("%Y-%m-%d", gmtime())
     data = data[data['date'] != currdate]
@@ -70,7 +69,7 @@ def SimpleStatistics(data, print=True):
         ratio = current / lastweek
         recentdbltime = round(7 * np.log(2) / np.log(ratio), 1)
         dailypercentchange = round(100 * (pow(ratio, 1 / 7) - 1), 1)
-        if print:
+        if print_stats:
             st.write('** Based on Most Recent Week of Data **')
             st.write('\tConfirmed cases on', co.index[-1], '\t', current)
             st.write('\tConfirmed cases on', co.index[-8], '\t', lastweek)
@@ -88,6 +87,14 @@ def SimpleStatistics(data, print=True):
 def regressions(x, y):
     logisticworked = False
     exponentialworked = False
+    logisticr2 = 0
+    expr2 = 0
+    ldoubletime = 0
+    ldoubletimeerror = 0
+    edoubletime = 0
+    edoubletimeerror = 0
+    epopt = 0
+    lpopt = 0
 
     try:
         lpopt, lpcov = curve_fit(logistic, x, y, maxfev=10000)
@@ -131,16 +138,11 @@ def regressions(x, y):
     except:
         pass
 
-        if round(logisticr2, 2) > round(expr2, 2):
-            return logisticr2, expr2, ldoubletime, ldoubletimeerror, edoubletime, edoubletimeerror, epopt, lpopt
-        else:
-            return logisticr2, expr2, ldoubletime, ldoubletimeerror, edoubletime, edoubletimeerror, epopt, lpopt
-
-    if logisticworked:
+    if round(logisticr2, 2) > round(expr2, 2):
+        return logisticr2, expr2, ldoubletime, ldoubletimeerror, edoubletime, edoubletimeerror, epopt, lpopt
+    elif logisticworked:
         return logisticr2, 0, ldoubletime, ldoubletimeerror, 0, 0, epopt, lpopt
-
-    if exponentialworked:
+    elif exponentialworked:
         return 0, expr2, 0, 0, edoubletime, edoubletimeerror, epopt, lpopt
-
     else:
         return 0, 0, float('NaN'), float('NaN'), float('NaN'), float('NaN'), epopt, lpopt
